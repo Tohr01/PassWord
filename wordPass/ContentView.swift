e3e5cca46b370af15d0a5be1c2c326be3ab40ad1
Tohr01 <carl@kratzundraabe.de>
2021-12-08 00:48:14 +0100

    Initial Commit

---
+++ wordPass/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  wordPass
+//
+//  Created by Carl Raabe on 08.12.21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ wordPass/wordPassApp.swift
@@ -0,0 +1,17 @@
+//
+//  wordPassApp.swift
+//  wordPass
+//
+//  Created by Carl Raabe on 08.12.21.
+//
+
+import SwiftUI
+
+@main
+struct wordPassApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
